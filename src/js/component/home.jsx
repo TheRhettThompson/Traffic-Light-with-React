939997fcc37936b4db1bebd80c62af8e527fb5b1
TheRhettThompson <rthompsoncareer@gmail.com>
2022-11-08 02:33:08 +0000

    purple button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [selectedColor , setSelectedColor] = useState("")
 	return (
 		<div className="center">
-			<div className="wire" > </div>
+			<div className="wire" > 
+			</div>
+
 			<div className="box">
-				<div className="red light"></div>
-				<div className="yellow light"></div>
-				<div className="green light"></div>
+
+				<div 
+				onClick={() => setSelectedColor ('red')}
+				className={"light red " + (selectedColor === "red" ? "ambient" : "")}>
+				</div>
+
+				<div 
+				onClick={() => setSelectedColor('yellow')}
+				className={"light yellow " + (selectedColor === "yellow" ? "ambient" : "")}>
+				</div>
+
+
+				<div 
+				onClick={() => setSelectedColor('green')}
+				className={"light green " + (selectedColor === 'green' ? "ambient" : "")}>
+				</div>
+
+				<button 
+				onClick={() => setSelectedColor('purple')}
+				className={"" + (selectedColor === "purple" ? "light purple ambient box2" : "")}>
+				<p>See Purple</p>
+				</button>
+
 			</div>
 
 
